a5da3c58d0a00dfb3beb9fa4d7fab2ffd7763e2a
MustapakulovAzamat <azamustapakylov6@gmail.com>
2026-06-23 19:07:22 +0600

    Refactor TypeTrack interface to include additional properties for artist and album details

--- types/track.ts
+++ types/track.ts
@@ -1,8 +1,21 @@
-export interface Track {
+export interface TypeTrack {
   id: number,
   title: string,
-  artist: string,
-  cover: string,
-  audioUrl: string,
-  duration: number
+  name: string,
+  link: string,
+  preview: string,
+  picture: string,
+  picture_medium: string,
+  artist: {
+    id: number,
+    name: string,
+    picture: string
+  },
+  album: {
+    id: number,
+    title: string,
+    cover: string,
+    cover_medium: string,
+    cover_small: string,
+  }
 }
