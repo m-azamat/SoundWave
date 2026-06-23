26731bae9f2afb4240283a6bb4807296b3c751b1
MustapakulovAzamat <azamustapakylov6@gmail.com>
2026-06-23 19:16:06 +0600

    Refactor layout structure in Home component for improved readability

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,34 @@
-
 import Playlist from "@/components/Tracks/Playlist";
 import Artist from "@/components/Tracks/Artist";
 import Albums from "@/components/Tracks/Albums";
 import Track from "@/components/Tracks/TrackList";
 import Title from "@/components/Text/Title";
 import Filter from "@/components/Filter";
-// import { useSession } from "next-auth/react";
 
 export default function Home() {
 
-  // const session = useSession();
-
   return (
     <main>
-      <div className="flex flex-col gap-10">
-        
-        
-          <div>
-            <Filter/>
-          </div>
-          <div>
-            <Title className="mb-4">Playlists</Title>
-            <Playlist />
-          </div>
-          <div>
-            <Title className="mb-4">Artists</Title>
-            <Artist/>
-          </div>
-          <div>
-            <Title className="mb-4">Albums</Title>
-            <Albums/>
-          </div>
-          <div>
-            <Title className="mb-4">Tracks</Title>
-            <Track/>
-          </div>
-        
-      
+      <div className="flex flex-col gap-10">        
+        <div>
+          <Filter/>
+        </div>
+        <div>
+          <Title className="mb-4">Playlists</Title>
+          <Playlist />
+        </div>
+        <div>
+          <Title className="mb-4">Artists</Title>
+          <Artist/>
+        </div>
+        <div>
+          <Title className="mb-4">Albums</Title>
+          <Albums/>
+        </div>
+        <div>
+          <Title className="mb-4">Tracks</Title>
+          <Track/>
+        </div>
       </div>
     </main>
   );
