c5134238ccd55abd6b0f4aa6fa71e010baaa4c1b
MustapakulovAzamat <azamustapakylov6@gmail.com>
2026-06-23 19:16:36 +0600

    Remove commented-out Credentials provider code from authConfig

--- configs/auth.ts
+++ configs/auth.ts
@@ -1,5 +1,4 @@
 import { AuthOptions } from "next-auth";
-// import Credentials from "next-auth/providers/credentials";
 import Google from "next-auth/providers/google";
 
 export const authConfig: AuthOptions = {
@@ -7,25 +6,6 @@ export const authConfig: AuthOptions = {
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string
-    }),
-    // Credentials({
-    //   credentials: {
-    //     email: {label: 'email', type: 'email', required: true},
-    //     password: {label: 'password', type: 'password', required: true}
-    //   },
-    //   async authorize(credentials) {
-    //     if(!credentials?.email || !credentials?.password) return null;
-
-    //     const currentUser = users.find(
-    //       user => user.email === credentials.email && 
-    //       user.password === credentials.password
-    //     );
-    //     if(currentUser) {
-    //       const {password, ...userWithoutPassword} = currentUser;
-    //       return userWithoutPassword as User;
-    //     }
-    //     return null;
-    //   }
-    // })
+    })
   ]
 }
