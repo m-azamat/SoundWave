925fead73c48e3c743b9773cc9503a13033cd5eb
MustapakulovAzamat <azamustapakylov6@gmail.com>
2026-06-23 19:06:53 +0600

    Refactor Player component to use AudioContext for audio management and improve playback handling

--- components/Player/Player.tsx
+++ components/Player/Player.tsx
@@ -1,22 +1,72 @@
 "use client"
-import { usePlayerStore } from "@/store/playerStore"
 
-export default function Player() {
-  const { currentTrack, isPlaying, togglePlay } = usePlayerStore()
+import { useEffect } from "react"
+import Text from "../Text/Text"
+import ButtonPlayer from "../ButtonPlayer"
+import Image from "next/image"
+import { useAudio } from "@/app/context/AudioContext"
 
-  // if (!currentTrack) return null
+export default function Player(){
+
+  const {currentTrack, isPlaying, audioRef, togglePlay} = useAudio();
+
+  useEffect(() => {
+    if(!currentTrack || !audioRef.current) return
+    if(audioRef.current.src !== currentTrack.preview) {
+      audioRef.current.src = currentTrack.preview
+      audioRef.current.load()
+    }
+
+    if(isPlaying) {
+      audioRef.current.play()
+    }
+  }, [currentTrack, audioRef, isPlaying])
+
+  useEffect(() => {
+    if(!audioRef.current) return
+
+    if(isPlaying) {
+      audioRef.current.play()
+    }else {
+      audioRef.current.pause()
+    }
+  }, [isPlaying, audioRef])
 
   return (
-    <div className="">
-      
-      <div className="ml-4">
-        <p></p>
-        <span className="text-sm text-gray-400"></span>
+    currentTrack && 
+    <div className={
+      "fixed left-0 bottom-0 bg-gray-900 w-full flex items-center justify-between p-6"
+    }>
+      <div>
+          <div className="flex flex-1 min-w-0 items-center gap-2">
+            <Image 
+              src={currentTrack?.album.cover_medium} 
+              alt={currentTrack?.artist.name} 
+              width={60}
+              height={60}
+              className="rounded-xl flex-none"
+            />
+            <div className="min-w-0 truncate">
+              <Text className="text-sm text-gray-400">
+                {currentTrack?.title}
+              </Text>
+              <Text className="text-sm text-gray-400">
+                {currentTrack?.artist.name}
+              </Text>
+            </div>
+          </div>
       </div>
-
-      <button onClick={togglePlay} className="mx-auto">
-        {isPlaying ? "⏸" : "▶️"}
-      </button>
+      <div className="flex-none w-20 flex justify-center">
+        <ButtonPlayer
+          isPlaying={isPlaying}
+          isActive={isPlaying}
+          onPlay={() => togglePlay()}
+        />
+        <audio ref={audioRef}></audio>
+      </div>
+      {/* <div>
+        <h1></h1>
+      </div> */}
     </div>
   )
 }
