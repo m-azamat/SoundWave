d9e5fee11e47cccc44a98de588bdf618d359f963
MustapakulovAzamat <azamustapakylov6@gmail.com>
2026-06-23 19:03:53 +0600

    Add GET route for Deezer API to fetch playlists, albums, and artists

---
+++ app/api/deezer/route.ts
@@ -0,0 +1,37 @@
+
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const {searchParams} = new URL(request.url);
+
+  const type = searchParams.get("type")
+  const id = searchParams.get("id")
+  let url = "";
+  
+  switch(type) {
+    case "playlists":
+      url = "https://api.deezer.com/chart/0/playlists?limit=15";
+      break;
+    case "playlist":
+      url = `https://api.deezer.com/playlist/${id}`;
+      break;
+    case "artists":
+      url = "https://api.deezer.com/chart/0/artists?limit=15";
+      break;
+    case "artist":
+      url = `https://api.deezer.com/artist/${id}/top?limit=50`;
+      break;
+    case "albums":
+      url = "https://api.deezer.com/chart/0/albums?limit=15";
+      break;
+    case "album":
+      url = `https://api.deezer.com/album/${id}`;
+      break;
+    default: 
+      return NextResponse.json({error: "Invalid type"}, {status: 400});
+  }
+  
+  const res = await fetch(url);
+  const data = await res.json();
+  return NextResponse.json(data);
+}
